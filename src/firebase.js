5f0da9b1bdd19abde9766c4ccdecd121c96ad804
Carmarí Barreto <carmaribarreto3@gmail.com>
2020-06-17 12:51:04 -0400

    Part one

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+export default{
+apiKey: "AIzaSyApglGm6qFN3ZZl6kF9ZRaSU6BNtx1ADek",
+    authDomain: "tdddg3.firebaseapp.com",
+    databaseURL: "https://tdddg3.firebaseio.com",
+    projectId: "tdddg3",
+    storageBucket: "tdddg3.appspot.com",
+    messagingSenderId: "623458659517",
+    appId: "1:623458659517:web:c5e7b25ab7c8fd709b5bd4",
+    measurementId: "G-5QJY1RKPZS"
+  };
