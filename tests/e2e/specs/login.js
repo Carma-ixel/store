7fae03c3ea90ae4c3d62b1ac9a8f14684fd2c1eb
Carmarí Barreto <carmaribarreto3@gmail.com>
2020-06-21 21:04:17 -0400

    part8 test.e2e

--- tests/e2e/specs/login.js
+++ tests/e2e/specs/login.js
@@ -1,11 +1,8 @@
 describe ('Login', () =>{
     it('Redirección a e home al login exitoso', () =>{
     cy.visit('http://localhost:8080/login')
-    cy.get('#email').type('user1@mystore.com')
-    cy.get('#password').type('password')
-    cy.get('button.is-primary').click()
-    cy.get('.navbar-burger').click()
-    cy.get('#navbarMenu').contains('Usuario')
-
+    cy.get('#email').type('carmabarreto@gmail.com')
+    cy.get('#password').type('12345**')
+    cy.get('button.is-primary.is-pulled-right').click() 
   })
 })
--- tests/e2e/specs/products.js
+++ tests/e2e/specs/products.js
@@ -1,30 +1,28 @@
 describe('Products page', () => {
     it('Filters products list', () =>{
-        cy.visit('http://localhost:8080')
-        cy.get('.column.is-4').should('have.length', 14)
+        cy.visit('/')
+        cy.get('.column.is-3').should('have.length', 0)
 
-        //Si busco Casa me debe traer la cantidad de column correspondiente a esa propiedad
-        cy.get('input').type('Casa')
-        cy.get('.column.is-4').should('have.length', 1)
+        //Si busco Producto, me debe traer la cantidad de column correspondiente a esa propiedad
+        cy.get('input').type('Flor de Jade')
+        cy.get('.column.is-3').should('have.length', 1)
 
         //Recetear on clear
-        cy.get('input').clear().type('Guitarra')
-        cy.get('.column.is-4').should('have.length', 0)
+        cy.get('input').clear().type('Orquídea Fantasma')
+        cy.get('.column.is-3').should('have.length', 1)
     })
 
-    it('Añadi al carritos', () =>{
-        cy.visit('http://localhost:8080')
-        cy.get('.navbar-burger').click()
+    it('Añadir al carrito', () =>{
+        cy.visit('/')
         cy.get('input').clear()
-        cy.get('.card button.is-pulled-right.is-warning').first().click()
+        cy.get('.card button.is-pulled-right.is-primary').first().click()
         cy.get('.navbar-burger').click()
         cy.get('span.tag.is-warning').contains('1')
     })
     
     it('Eliminar del carrito', () =>{
-        cy.visit('http://localhost:8080')
-        cy.get('.navbar-burger').click()
-        cy.get('.card button.is-pulled-right.is-warning').first().click()
+        cy.visit('/')
+        cy.get('.card button.is-pulled-right.is-primary').first().click()
         cy.get('.navbar-burger').click()
         cy.get('[data-testID="cart"]').click()
         cy.get('.modal-card-body').find('div.card-content').should('have.length', 1)
